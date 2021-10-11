66ae9447ca31b7d5fcdef7f3974292bc839bf9c5
kamkonphrom <kamkonphrom@gmail.com>
2021-10-11 21:59:32 +0700

    get movie list in admin panel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -10,44 +17,45 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
-
+import { AuthContext } from "./context/authContext/AuthContext";
 
 function App() {
-  
-
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-    <Switch>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/movies">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-      </div>
-      </>
+      <Switch>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        {user ? (
+          <>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/movies">
+                <ProductList />
+              </Route>
+              <Route path="/product/:productId">
+                <Product />
+              </Route>
+              <Route path="/newproduct">
+                <NewProduct />
+              </Route>
+            </div>
+          </>
+        ) : (
+          <Redirect to="/login" />
+        )}
       </Switch>
     </Router>
   );
--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -4,43 +4,46 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function WidgetSm() {
-  const [newUsers, setNewUsers] = useState([])
+  const [newUsers, setNewUsers] = useState([]);
 
   useEffect(() => {
-    const getNewUsers = async ()=>{
-      try{
-      const res = await axios.get("/users?new=true", {
-        headers: {
-          token: 
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWIwNjFkZTRkNWNjYmUxZjI5ZmJiOSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzQ0MDM4MiwiZXhwIjoxNjMzODcyMzgyfQ.ifduORg62tnWl2MeMeLI8TrIgf5mIBHiSIEla3RPF8o"
-        },
-      });
-      setNewUsers(res.data)
-      }catch(err){
-        console.log(err)
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWIwNjFkZTRkNWNjYmUxZjI5ZmJiOSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzQ0MDM4MiwiZXhwIjoxNjMzODcyMzgyfQ.ifduORg62tnWl2MeMeLI8TrIgf5mIBHiSIEla3RPF8o",
+          },
+        });
+        setNewUsers(res.data);
+      } catch (err) {
+        console.log(err);
       }
-    }
+    };
     getNewUsers();
-  },[]);
+  }, []);
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
       <ul className="widgetSmList">
-      {newUsers.map(user => (
-        <li className="widgetSmListItem">
-          <img
-            src={user.profilePic || "https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg"}
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">{user.username}</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
+        {newUsers.map((user) => (
+          <li className="widgetSmListItem">
+            <img
+              src={
+                user.profilePic ||
+                "https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
         ))}
       </ul>
     </div>
