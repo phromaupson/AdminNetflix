63fa2541d6ff3bc9750804c241d6e4dffbcb9848
kamkonphrom <kamkonphrom@gmail.com>
2021-10-17 21:16:26 +0700

    Add CRUD feature in admin panel

---
+++ src/pages/newList/NewList.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useContext } from "react";
+import {useHistory} from 'react-router-dom'
+
+import "./newList.css";
+import { createMovie, getMovies } from "../../context/movieContext/apiCalls";
+import { ListContext } from "../../context/listContext/ListContext";
+import { MovieContext } from "../../context/movieContext/movieContext";
+import { useEffect } from "react";
+import { createList } from "../../context/listContext/apiCalls";
+
+export default function NewList() {
+  const [list, setList] = useState(null);
+
+  const history = useHistory()
+
+  const { dispatch } = useContext(ListContext);
+  const { movies, dispatch: dispatchMovie } = useContext(MovieContext);
+
+  useEffect(() => {
+    getMovies(dispatchMovie)
+  }, [dispatchMovie])
+
+  const handleSelect = (e) => {
+    let value = Array.from(e.target.selectedOptions, (option) => option.value)
+    setList({...list, [e.target.name]: value})
+  }
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setList({ ...list, [e.target.name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createList(list, dispatch)
+    history.push('/lists')
+  };
+
+  return (
+    <div className="newProduct">
+      <h1 className="addProductTitle">New List</h1>
+      <form className="addProductForm">
+        <div className="formLeft">
+          <div className="addProductItem">
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Popular Movies"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="addProductItem">
+            <label>Genre</label>
+            <input
+              type="text"
+              name="genre"
+              placeholder="action"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="addProductItem">
+            <label>Type</label>
+            <select name="type" onChange={handleChange}>
+            <option>Type</option>
+              <option value="movie">Movie</option>
+              <option value="series">Series</option>
+            </select>
+          </div>
+        </div>
+        <div className="formRight">
+          <div className="addProductItem">
+            <label>Content</label>
+            <select multiple name="content" onChange={handleSelect} style={{height: "280px"}}>
+              {movies.map((movie) => (
+                <option key={movie._id} value={movie._id}>{movie.title}</option>
+              ))}
+              
+            </select>
+          </div>
+        </div>
+          <button className="addProductButton" onClick={handleSubmit}>
+            Create
+          </button>
+      </form>
+    </div>
+  );
+}
